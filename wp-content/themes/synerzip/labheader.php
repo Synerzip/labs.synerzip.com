9a2bda2752662ebb664bd97017e0d73011becbfd
maheshb <maheshb@ULTP_371>
2018-07-04 11:56:55 +0530

    set home page

--- wp-content/themes/synerzip/labheader.php
+++ wp-content/themes/synerzip/labheader.php
@@ -385,13 +385,7 @@ elseif ($post->post_type == 'webinar') {
                       
                     </nav>
                     <div class="navbar-header" >
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a href="<?php echo $siteUrl;?>/" class="navbar-brand"><img src="<?php bloginfo('template_url'); ?>/beta_images/<?php echo $logoColor; ?>" title="Synerzip" alt="Synerzip" width="190" /></a>
+                       <a href="<?php echo $siteUrl;?>/" class="navbar-brand"><img src="<?php bloginfo('template_url'); ?>/beta_images/<?php echo $logoColor; ?>" title="Synerzip" alt="Synerzip" width="190" /></a>
                     </div>
 
 
