b4728cf5f99f31da7cc6d29329be71d19264e136
maheshb <maheshb@ULTP_371>
2018-07-10 12:08:11 +0530

    deleted unwant file

--- wp-content/themes/synerzip/searchform.php
+++
@@ -1,5 +0,0 @@
-<form role="search" method="get" id="site_search" action="<?php echo get_option('home'); ?>/" >
-    <input type="text" name="s" id="s" class="text" placeholder="Search Site" value="" />
-    <button id="search_submit" type="submit"><i class="fa fa-search" aria-hidden="true"></i>
-</button>
-</form>
--- wp-content/themes/synerzip/single-testimonial.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
-* @package WordPress
- * @subpackage Synerzip
-*/
-
-header( 'Location: //' . $_SERVER['SERVER_NAME']  ) ;
-?>
